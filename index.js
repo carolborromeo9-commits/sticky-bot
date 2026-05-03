38dd5b658a768eb6c1b675f83f36119ad611c285
lucianoire <carolborromeo9@gmail.com>
2026-05-03 11:22:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,27 +9,35 @@ const client = new Client({
 });
 
 const STICKY_CHANNEL_ID = "1455613452390236348";
-let lastSticky = null;
 
-client.on('messageCreate', async (message) => {
-  if (message.author.bot) return;
-
-  // dito lang siya gagana
-  if (message.channel.id !== STICKY_CHANNEL_ID) return;
-
-  if (lastSticky) {
-    try {
-      await lastSticky.delete();
-    } catch {}
-  }
-
-  lastSticky = await message.channel.send(`_ _
+const stickyText = `_ _
 -# _ _             **ping <@&1455613450935079116> for inquiries** <a:korila:1482440638694686811> 
 -# _ _             <:pearl:1483100606649602159>do     censor     item    names
 -# _ _             <:pearl:1483100606649602159>ex. c4nva, b00st, r0bux, gn6
 
 -# _ _ _ _                  <#1455613451903832274> **for orders**
-_ _`);
+_ _`;
+
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return;
+  if (message.channel.id !== STICKY_CHANNEL_ID) return;
+
+  try {
+    const messages = await message.channel.messages.fetch({ limit: 20 });
+
+    const oldStickies = messages.filter(m =>
+      m.author.id === client.user.id &&
+      m.content.includes("ping <@&1455613450935079116>")
+    );
+
+    for (const sticky of oldStickies.values()) {
+      await sticky.delete().catch(() => {});
+    }
+
+    await message.channel.send(stickyText);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 client.login(process.env.TOKEN);
